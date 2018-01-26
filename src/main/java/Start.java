30e49071f835476034be34c15836a4ec599f0c82
CloseRa <krotokob@mail.ru>
2018-01-26 20:14:42 +0400

    1

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,8 +1,15 @@
-/**
- * Created by Vlad on 26.01.2018.
- */
+
 public class Start {
     public static void main(String[]args){
+        int [] a;
+        a = new int[6];
+        a[0]=5;
+        a[1]=6;
+        a[2]=1;
+        a[3]=2;
+        a[4]=3;
+        a[5]=12;
+        a[6]=15;
         System.out.println("1");
     }
 }
