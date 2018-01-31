44beac499ebcf51e370042b0f1c478c1a0ab0aea
CloseRa <krotokob@mail.ru>
2018-01-31 16:05:19 +0400

    3

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,28 +1,8 @@
-import java.util.Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Start {
     public static void main(String[] args) {
-        int e[] = {89, 2, 5, 9, 14, 42, 12, 43, 23, 40};
-        int min = min(e);
-        int max = max(e);
-        System.out.println("Max value = " + max + " Min value = " + min);
-
-    }
-
-    private static int min(int[] e) {
-        int min = e[0];
-        for (int anE : e) {
-            if (anE < min)
-                min = anE;
-        }
-        return min;
-    }
-
-    private static int max(int[] e) {
-        int max = e[0];
-        for (int anE : e) {
-            if (max < anE) max = anE;
-        }
-        return max;
     }
 }
