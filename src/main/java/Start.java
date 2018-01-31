7b39dce779dce06a38530803f567f0fdefe71787
CloseRa <krotokob@mail.ru>
2018-01-31 17:03:07 +0400

    3

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 public class Start {
     public static void main(String[] args) {
+        ParkingArea parkingArea = new ParkingArea();
         Car sportCar = new SportCar("Audi v8", 3, 777);
         Car track = new Track("Mercedes IO", 5, 896);
         Car bus = new Bus("Toyota", 7, 903);
@@ -12,5 +13,8 @@ public class Start {
         Driver driver2 = new Driver("John", 893021);
         Driver driver3 = new Driver("Gog", 762312);
 
+        parkingArea.addCar(driver1, track);
+        parkingArea.addCar(driver2, sportCar);
+        parkingArea.addCar(driver3, bus);
     }
 }
